121b3040c7be3879ff67ec0d3e771968aec813b4
Iryna Shumovska <shumovska_ia@groupbwt.com>
2023-12-12 01:04:16 +0200

    Chameleon effect

--- js/index.js
+++ js/index.js
@@ -2,3 +2,13 @@
 window.addEventListener('scroll', e => {
     document.body.style.cssText = `--scrollTop: ${this.scrollY}px`
 })
+
+// Chameleon
+const colorPicker = document.getElementById("color-picker");
+const chameleon = document.getElementById("chameleon");
+
+const chameleonBody = document.getElementById("background_chameleon");
+colorPicker.addEventListener("change", function () {
+  const chosenColor = this.value;
+  chameleonBody.style.backgroundColor = chosenColor;
+});
